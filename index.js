0756375cc1fd9131116b7be558a7bd80fca92bc4
crystal-csete <68755171+crystal-csete@users.noreply.github.com>
2020-09-08 16:02:39 -0600

    added

--- index.js
+++ index.js
@@ -1,33 +1,56 @@
 /************************************************************** Task 1: Warm-up! **************************************************************/
 //Task a: declare a variable called votingAge, console log true if age > 18 (no function required)
 
+let votingAge = 18;
+
+if (votingAge >= 18) {
+    console.log(true);
+}else {
+    console.log(false);
+}
 
 
 
 
 //Task b: declare a variable and then use a conditional to change the value of that variable based on the value assigned to a second variable (no function required)
 
+let thisIdea = "testing";
 
+console.log(thisIdea);
 
+thisIdea = "this thing";
+
+console.log(thisIdea);
+ 
 
 
 //Task c: Convert string ("1999") to integer (1999)  (no function required) // hint look up the Number method
 
 
 
+console.log(Number("1999"));
 
 
 //Task d: Write a function to multiply a*b 
 
 
+function multiply(a, b) {
+   return a * b;
+}
 
+console.log(multiply(5,5));
 
 
 /************************************************************** Task 2 **************************************************************/
 //Age in Dog years
 //write a function that takes your age and returns it to you in dog years - they say that 1 human year is equal to seven dog years 
 
+function convertedAge(myAge) {
+    let dogAge = 7 * myAge;
+    console.log(`${dogAge} is your dogs age and my age would be ${myAge}`);
+}
 
+convertedAge(36);
 
 
 
@@ -53,6 +76,9 @@
 
 
 
+
+
+
 /************************************************************** Task 4 **************************************************************/
 // Rock, Paper, Sissors
 // Your function should take a string (either rock paper or sissors)
@@ -60,20 +86,45 @@
 // use math.random to determine the computers choice 
 // hint while you can complete this with only conditionals based on strings it may help to equate choice to a number 
 
-  
+let computer = Math.random();
+
+function randomChoice() {
+    if ( computer <= .33) {
+        return "rocks";
+    }
+    else if ( computer <= .66) {
+        return "paper";
+    }
+    else if ( computer >= .67) {
+        return "scissors";
+    }
+}
+
+console.log(randomChoice(computer));
+
+
+
   
 
 /************************************************************** Task 5 **************************************************************/
 //Metric Converter
 //a. KM to Miles - should take the number of kilometers and convert it to the equal number of miles
 
+ let convertedMiles = 0.621371;
+ let kilometers = 1;
 
+ let miles = kilometers * convertedMiles;
 
-
+ console.log(`${kilometers} kilometers is equal to ${miles} miles.`);
 
 //b. Feet to CM - should take the number of feet and convert it to the equal number of centimeters
   
+let convertedCentimeters = 2.54;
+let inches = 1;
 
+let centimeters = inches * convertedCentimeters;
+
+console.log(`${inches} inches is equal to ${centimeters} centimeters.`)
 
 
 
@@ -82,6 +133,10 @@
 // create a function called annoyingSong
 // the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
   
+
+  
+
+
 
 
 
@@ -96,7 +151,29 @@
 //and anything below 60 should be F
   
 
-  
+function gradesForYou() {
+    for ( let i = 100; i > 0; i++){
+
+   
+    if ( i >=90) {
+        return "A";
+    }
+    else if ( i >=80) {
+        return "B";
+    }
+    else if ( i >=70) {
+        return "C";
+    }
+    else if ( i >=60) {
+        return "D";
+    }
+    else {
+        return "F";
+    }
+}
+  }
+console.log(gradesForYou);
+
   
 
 /************************************************************** Stretch **************************************************************/
@@ -111,7 +188,6 @@
 /************************************************************** Stretch **************************************************************/
 //Take Rock, Paper, Sissors further
 //update your rock papers sissors code below to take a prompt from a user using the window object
-
 
 
 
